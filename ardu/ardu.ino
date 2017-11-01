95ba3bd299bd66e1bc91e96dfc039a0668ab4b25
Mariusmivw <mariusmivw@gmail.com>
2017-11-01 16:52:35 +0100

    added sensor sensorness

--- ardu/ardu.ino
+++ ardu/ardu.ino
@@ -1,11 +1,28 @@
 // door Marius
+#include "MeMCore.h"
+
+MeUltrasonicSensor ultraSensor(PORT_3);
+MeLineFollower lineFinder(PORT_2);
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
+
 void setup(){
-  inputPins = [1, 4];
-  for (size_t i = 0; i < len(inputPins); i++) {
-    pinMode(inputPins[i], INPUT);
-  }
+  Serial.begin(9600);
 }
 
 void loop() {
   //loop
+  //int val = lineFinder.readSensors();
+  int val = ultraSensor.distanceCm();
+  Serial.println(val);
+  /*switch(val)
+  {
+    case S1_IN_S2_IN: Serial.println("Sensor 1 and 2 are inside of black line"); break; motor1.
+    case S1_IN_S2_OUT: Serial.println("Sensor 2 is outside of black line"); break;
+    case S1_OUT_S2_IN: Serial.println("Sensor 1 is outside of black line"); break;
+    case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); break;
+    default: break;
+  }*/
+  delay(200);
 }
+
