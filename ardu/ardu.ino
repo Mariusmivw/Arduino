c7525b45457cb47265c4b75ba70e524d48dd2d94
Mariusmivw <mariusmivw@gmail.com>
2017-11-10 12:29:39 +0100

    added driving

--- ardu/ardu.ino
+++ ardu/ardu.ino
@@ -3,8 +3,9 @@
 
 MeUltrasonicSensor ultraSensor(PORT_3);
 MeLineFollower lineFinder(PORT_2);
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
+MeDCMotor motor1(M1); //left
+MeDCMotor motor2(M2); //right
+uint8_t fastness = 100;
 
 void setup(){
   Serial.begin(9600);
@@ -14,15 +15,21 @@ void loop() {
   //loop
   int val = lineFinder.readSensors();
   //int val = ultraSensor.distanceCm();
-  Serial.println(val); // l + r = 2 + 1
-  switch(val)
-  {
-    case 0: Serial.println("Sensor 1 and 2 are inside of black line"); break; motor1.
-    case 1: Serial.println("Sensor 2 is outside of black line"); break;
-    case 2: Serial.println("Sensor 1 is outside of black line"); break;
-    case 3: Serial.println("Sensor 1 and 2 are outside of black line"); break;
-    default: break;
+  //Serial.println(val); // l + r = 2 + 1
+  if (val == 1){
+    Serial.println("Sensor 1 is outside of black line");
+    motor1.run(fastness/2);
+    motor2.run(fastness/2);
   }
-  delay(200);
+  else if (val == 2){
+    Serial.println("Sensor 2 is outside of black line");
+    motor1.run(-fastness/2);
+    motor2.run(-fastness/2);
+  }
+  else{
+    Serial.println("Sensor 1 and 2 are outside of black line");
+    motor1.run(-fastness);
+    motor2.run(fastness);
+  }
 }
 
