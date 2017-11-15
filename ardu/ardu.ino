496c56353c4a4029af1a4291834bdf1b2e9bf307
Mariusmivw <mariusmivw@gmail.com>
2017-11-15 17:11:38 +0100

    Sure

--- ardu/ardu.ino
+++ ardu/ardu.ino
@@ -3,8 +3,11 @@
 
 MeUltrasonicSensor ultraSensor(PORT_3);
 MeLineFollower lineFinder(PORT_2);
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
+MeDCMotor motor1(M1); //left
+MeDCMotor motor2(M2); //right
+int fastness1 = -190;
+int fastness2 = -fastness1;
+int t;
 
 void setup(){
   Serial.begin(9600);
@@ -12,17 +15,45 @@ void setup(){
 
 void loop() {
   //loop
-  //int val = lineFinder.readSensors();
-  int val = ultraSensor.distanceCm();
-  Serial.println(val);
-  /*switch(val)
-  {
-    case S1_IN_S2_IN: Serial.println("Sensor 1 and 2 are inside of black line"); break; motor1.
-    case S1_IN_S2_OUT: Serial.println("Sensor 2 is outside of black line"); break;
-    case S1_OUT_S2_IN: Serial.println("Sensor 1 is outside of black line"); break;
-    case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); break;
-    default: break;
-  }*/
-  delay(200);
+  int val = lineFinder.readSensors();
+  
+  //int val = ultraSensor.distanceCm();
+  //Serial.println(val); // l + r = 2 + 1
+  if (val == 0){
+    Serial.println("Sensor 1 and 2 are inside of black line");
+    t = 0;
+    motor1.run(fastness1);
+    motor2.run(fastness2);
+  }
+  else if (val == 1){
+    Serial.println("Sensor 1 is outside of black line"); //right
+    t = 1;
+    motor1.run(fastness1);
+    motor2.run(fastness2);
+  }
+  else if (val == 2){
+    Serial.println("Sensor 2 is outside of black line"); //left
+    t = 2;
+    motor1.run(fastness1);
+    motor2.run(fastness2);
+  }
+  else{
+    Serial.println("Sensor 1 and 2 are outside of black line");
+    if (t == 0 | t == 3){
+      t = 3;
+      motor1.run(-fastness1/2);
+      motor2.run(-fastness2/2);
+    }
+    else if (t == 1){
+      // naar links
+      motor1.run(0);
+      motor2.run(fastness2);
+    }
+    else{
+      // naar rechts
+      motor1.run(fastness1);
+      motor2.run(0);
+    }
+  }
 }
 
